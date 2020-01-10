80ba867228e0d33a63ed488b23d8d13d5b1a1063
Rose <apontera2111@aguafria.org>
2020-01-10 14:44:11 -0700

    done

---
+++ tny_timer.js
@@ -0,0 +1,49 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Review Assignment
+
+   Event Timer
+   Author: Rose Aponte
+   Date:   1-9-20
+
+*/
+"use strict";
+//calling function
+showClock();
+setInterval("showClock()", 1000);
+showClock();
+
+function showClock(){
+   
+   var thisDay = new Date();
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+   document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span><span>" + localTime + "</span>";
+   var j4Date = nextJuly4(thisDay);
+   j4Date.setHours("21");
+
+   //Calculate time left and change text
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   var hrs = (days - Math.floor(days))*24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
+   document.getElementById("dLeft").textContent = Math.floor(days);
+   document.getElementById("hLeft").textContent = Math.floor(hrs);
+   document.getElementById("mLeft").textContent = Math.floor(mins);
+   document.getElementById("sLeft").textContent = Math.ceil(secs);
+
+
+}
+
+
+
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
+
+
